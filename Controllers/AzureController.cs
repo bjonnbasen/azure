77c693b59dc7dc6ffa689bd95d5e335f9b2cbf57
Liaquat Ali <liaquat.ali@bouvet.no>
2019-01-30 00:11:22 +0100

    changed

--- Controllers/AzureController.cs
+++ Controllers/AzureController.cs
@@ -4,18 +4,28 @@
 
 using Microsoft.AspNetCore.Mvc;
 
-namespace azure.Controllers{
-[Route("[controller]")]
-public class AzureController : Controller{
+namespace azure.Controllers
+{
+    [Route("[controller]")]
+    public class AzureController : Controller
+    {
 
-    [HttpGet]
-    public string GET(){
+        [HttpGet]
+        public string GET()
+        {
 
-        return "Ali";
+            return "Ali";
+        }
+
+        [HttpPost]
+        public string Add(int t)
+        {
+            return "value added";
+
+        }
+
+
     }
-
-
-}
 
 
 }
